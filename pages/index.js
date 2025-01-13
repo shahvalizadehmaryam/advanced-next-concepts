eff9a866754809969dc785a0a8ca93fb14fec35d
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-01-13 09:28:26 +0330

    title and meta tag desc added to app.

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Botostart</title>
+        <meta name="Botostart" content="botostart is a educational website" />
+      </Head>
+      <h3>Home page</h3>
+    </div>
+  );
+}
---
+++ pages/next.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const NextJS = () => {
+  return (
+    <div>
+      <Head>
+        <title>Nextjs course</title>
+        <meta name="nextJs course" content="botostart has next.js course" />
+      </Head>
+    </div>
+  );
+};
+
+export default NextJS;
