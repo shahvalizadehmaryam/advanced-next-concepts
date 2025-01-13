5555340de62b6d38efc6d4d81c97cc7f665e26c6
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-01-13 09:57:37 +0330

    title and meta tag become dynamic.

--- pages/next.js
+++ pages/next.js
@@ -1,14 +1,27 @@
 import Head from "next/head";
 
-const NextJS = () => {
+const NextJS = ({ data }) => {
+  console.log(data);
   return (
     <div>
       <Head>
-        <title>Nextjs course</title>
-        <meta name="nextJs course" content="botostart has next.js course" />
+        <title>{data.title}</title>
+        <meta name="nextJs course" content={data.description} />
       </Head>
+      <h1>next.js page - {data.name}</h1>
     </div>
   );
 };
 
 export default NextJS;
+export async function getServerSideProps() {
+  return {
+    props: {
+      data: {
+        name: "maryam",
+        title: "maryam page",
+        description: "maryam is a programmer",
+      },
+    },
+  };
+}
