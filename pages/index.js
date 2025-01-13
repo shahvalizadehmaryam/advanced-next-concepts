a79d3453514db859944ec17cb8f4849eb9269564
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-01-13 10:14:02 +0330

    avoid repeating head component.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Botostart</title>
-        <meta name="Botostart" content="botostart is a educational website" />
-      </Head>
       <h3>Home page</h3>
     </div>
   );
