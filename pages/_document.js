be33a71f0c995c0f57d362355f578be04e784a52
Maryam Shahvalizadeh <Shahvalizadehmaryam@gmail.com.>
2025-01-13 12:30:41 +0330

    language of project changed to fa.:

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,12 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="fa" dir="rtl">
         <Head>
           <meta name="author" contant="Maryam shahvalizaeh" />
         </Head>
         <body>
-          <h1>Maryam</h1>
+          <h1>مریم</h1>
           <Main />
           <NextScript />
         </body>
